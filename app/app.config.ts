e5df294956676dd77da3eb45742b3acdc91be6e5
Rasool <rasoolol.77@gmail.com>
2025-12-25 21:46:22 +0330

    feat: update linkedin url

--- app/app.config.ts
+++ app/app.config.ts
@@ -18,28 +18,33 @@ export default defineAppConfig({
       slots: {
         container: 'py-18 sm:py-24 lg:py-32',
         title: 'mx-auto max-w-xl text-pretty text-3xl sm:text-4xl lg:text-5xl',
-        description: 'mt-2 text-md mx-auto max-w-2xl text-pretty sm:text-md text-muted'
+        description:
+          'mt-2 text-md mx-auto max-w-2xl text-pretty sm:text-md text-muted'
       }
     }
   },
   footer: {
     credits: `Lee-One Team • © ${new Date().getFullYear()}`,
     colorMode: false,
-    links: [{
-      'icon': 'i-simple-icons-telegram',
-      'to': 'tg://resolve?domain=mahsa_prode',
-      'target': '_blank',
-      'aria-label': 'Nuxt on Discord'
-    }, {
-      'icon': 'i-simple-icons-linkedin',
-      'to': 'https://www.linkedin.com/in/askari-mahsa',
-      'target': '_blank',
-      'aria-label': 'Nuxt on X'
-    }, {
-      'icon': 'i-simple-icons-github',
-      'to': 'https://github.com/askari-mahsa',
-      'target': '_blank',
-      'aria-label': 'Nuxt UI on GitHub'
-    }]
+    links: [
+      {
+        'icon': 'i-simple-icons-telegram',
+        'to': 'tg://resolve?domain=mahsa_prode',
+        'target': '_blank',
+        'aria-label': 'Nuxt on Discord'
+      },
+      {
+        'icon': 'i-simple-icons-linkedin',
+        'to': 'https://www.linkedin.com/in/mahsa-askari-926484117',
+        'target': '_blank',
+        'aria-label': 'Nuxt on X'
+      },
+      {
+        'icon': 'i-simple-icons-github',
+        'to': 'https://github.com/askari-mahsa',
+        'target': '_blank',
+        'aria-label': 'Nuxt UI on GitHub'
+      }
+    ]
   }
 })
